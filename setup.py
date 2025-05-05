0badc6f3c2d63ee26766f6a21ef2722d3d716840
Lubomír Sedlář <lsedlar@redhat.com>
2025-05-05 15:48:56 +0200

    Release 4.9.2
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ packages = sorted(packages)
 
 setup(
     name="pungi",
-    version="4.9.1",
+    version="4.9.2",
     description="Distribution compose tool",
     url="https://pagure.io/pungi",
     author="Dennis Gilmore",
