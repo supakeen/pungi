1116e1f278ea547cf3bf9265eba70d7a8aa4795e
Lubomír Sedlář <lsedlar@redhat.com>
2025-06-10 09:20:04 +0200

    Specify data_files with relative paths
    The paths should be relative to sys.prefix, which happens to be /usr in
    the RPM world. This change should make installation with
    %pyproject_install macro from a generated wheel work correctly.
    
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- setup.py
+++ setup.py
@@ -45,11 +45,11 @@ setup(
     },
     scripts=["contrib/yum-dnf-compare/pungi-compare-depsolving"],
     data_files=[
-        ("/usr/lib/tmpfiles.d", glob.glob("contrib/tmpfiles.d/*.conf")),
-        ("/usr/share/pungi", glob.glob("share/*.xsl")),
-        ("/usr/share/pungi", glob.glob("share/*.ks")),
-        ("/usr/share/pungi", glob.glob("share/*.dtd")),
-        ("/usr/share/pungi/multilib", glob.glob("share/multilib/*")),
+        ("lib/tmpfiles.d", glob.glob("contrib/tmpfiles.d/*.conf")),
+        ("share/pungi", glob.glob("share/*.xsl")),
+        ("share/pungi", glob.glob("share/*.ks")),
+        ("share/pungi", glob.glob("share/*.dtd")),
+        ("share/pungi/multilib", glob.glob("share/multilib/*")),
     ],
     test_suite="tests",
     install_requires=[
